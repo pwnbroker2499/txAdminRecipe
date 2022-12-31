6df508f5e1a77759411f3a8d5e1edd6c84cdbbbd
RexShack <rexshackuk@gmail.com>
2022-12-31 15:07:48 +0000

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -261,3 +261,12 @@ CREATE TABLE `player_plants` (
   `plantid` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `job_wagons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job` varchar(50) DEFAULT NULL,
+  `plate` varchar(255) NOT NULL,
+  `wagon` varchar(50) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
