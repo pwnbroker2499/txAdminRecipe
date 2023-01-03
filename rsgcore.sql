cd7b9ecb68dc23c6ffbb2030c436af7aa08044d7
RexShack <rexshackuk@gmail.com>
2023-01-03 18:42:59 +0000

    add rsg-horses

--- rsgcore.sql
+++ rsgcore.sql
@@ -246,13 +246,23 @@ CREATE TABLE IF NOT EXISTS `telegrams` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `player_horses` (
+CREATE TABLE IF NOT EXISTS `player_horses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) NOT NULL,
-  `selected` int(11) NOT NULL DEFAULT 0,
-  `model` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `components` varchar(5000) NOT NULL DEFAULT '{}',
+  `citizenid` varchar(50) DEFAULT NULL,
+  `horseid` varchar(6) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `horse` varchar(50) DEFAULT NULL,
+  `horsexp` int(11) NOT NULL,
+  `saddle` varchar(50) DEFAULT NULL,
+  `blanket` varchar(50) DEFAULT NULL,
+  `saddlebag` varchar(50) DEFAULT NULL,
+  `bedroll` varchar(50) DEFAULT NULL,
+  `horn` varchar(50) DEFAULT NULL,
+  `stirrup` varchar(50) DEFAULT NULL,
+  `mane` varchar(50) DEFAULT NULL,
+  `tail` varchar(50) DEFAULT NULL,
+  `mask` varchar(50) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
