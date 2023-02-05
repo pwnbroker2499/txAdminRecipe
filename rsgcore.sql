4da80c4273b601580ce969ad74965e8f6811d77f
RexShack <rexshackuk@gmail.com>
2023-02-05 17:12:59 +0000

    add rsg-hotel

--- rsgcore.sql
+++ rsgcore.sql
@@ -304,3 +304,13 @@ CREATE TABLE IF NOT EXISTS `player_weapons` (
   `components` varchar(4096) NOT NULL DEFAULT '{}',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `player_rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) DEFAULT NULL,
+  `location` varchar(30) NOT NULL,
+  `credit` int(11) NOT NULL DEFAULT 0,
+  `roomid` int(11) DEFAULT NULL,
+  `active` int(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
