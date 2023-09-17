c6e4fddf04049bd900a99be4d6503de0169d903b
RexshackGaming <rexshackuk@gmail.com>
2023-09-17 12:01:08 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -126,3 +126,11 @@ CREATE TABLE IF NOT EXISTS `address_book` (
 `name`  VARCHAR(50) NOT NULL,
 `owner`  VARCHAR(50) NOT NULL,
 PRIMARY KEY (`id`));
+
+CREATE TABLE IF NOT EXISTS `weaponsmith_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `weaponsmith` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `item` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
