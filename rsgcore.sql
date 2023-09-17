8e5ae7c6826f78332449bb8b5bc9d2550da37186
RexshackGaming <rexshackuk@gmail.com>
2023-09-17 16:13:21 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -134,3 +134,24 @@ CREATE TABLE IF NOT EXISTS `weaponsmith_stock` (
   `stock` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `weapon_shop` (
+  `shopid` varchar(255) NOT NULL,
+  `jobaccess` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `displayname` varchar(255) NOT NULL,
+  `money` double(11,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`shopid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `weapon_shop` (`shopid`, `jobaccess`, `displayname`, `money`) VALUES
+('valweaponshop', 'valweaponsmith', 'Valentine Weapon Shop', 0),
+('rhoweaponshop', 'rhoweaponsmith', 'Rhodes Weapon Shop', 0);
+
+CREATE TABLE `weapon_stock` (
+  `id` int(11) NOT NULL,
+  `shopid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `items` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `stock` int(11) NOT NULL DEFAULT 0,
+  `price` double(11,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
