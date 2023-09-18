3c9bd961ffc301202e3fbf15fa05af2c45b70f09
RexshackGaming <rexshackuk@gmail.com>
2023-09-18 08:09:55 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -151,7 +151,7 @@ INSERT INTO `weapon_shop` (`shopid`, `jobaccess`, `displayname`, `money`) VALUES
 ('annweaponshop', 'annweaponsmith', 'Annesburg Weapon Shop', 0);
 
 CREATE TABLE `weapon_stock` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `shopid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `items` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `stock` int(11) NOT NULL DEFAULT 0,
