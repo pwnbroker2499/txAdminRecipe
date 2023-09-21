8cde858a2570668ad0112031b2a456b926901431
RexshackGaming <rexshackuk@gmail.com>
2023-09-21 13:12:38 +0100

    add rsg-blacksmith

--- rsgcore.sql
+++ rsgcore.sql
@@ -159,6 +159,34 @@ CREATE TABLE `weapon_stock` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `blacksmith_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blacksmith` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `item` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `blacksmith_shop` (
+  `shopid` varchar(255) NOT NULL,
+  `jobaccess` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `displayname` varchar(255) NOT NULL,
+  `money` double(11,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`shopid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `blacksmith_shop` (`shopid`, `jobaccess`, `displayname`, `money`) VALUES
+('valblacksmithshop', 'valblacksmith', 'Valentine Blacksmith Shop', 0);
+
+CREATE TABLE `blacksmithshop_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shopid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `items` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `stock` int(11) NOT NULL DEFAULT 0,
+  `price` double(11,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `management_funds` (
 `id` INT(11) NOT NULL AUTO_INCREMENT,
 `job_name` VARCHAR(50) NOT NULL,
