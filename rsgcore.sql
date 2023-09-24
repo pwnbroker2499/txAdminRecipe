c64d031b554d77bffcc1ff12f17552b9efacce36
RexshackGaming <rexshackuk@gmail.com>
2023-09-24 17:59:31 +0100

    add stuff

--- rsgcore.sql
+++ rsgcore.sql
@@ -291,3 +291,11 @@ CREATE TABLE `saloontendershop_stock` (
   `price` double(11,2) NOT NULL DEFAULT 0.00,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `job_wagons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job` varchar(50) DEFAULT NULL,
+  `plate` varchar(255) NOT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
