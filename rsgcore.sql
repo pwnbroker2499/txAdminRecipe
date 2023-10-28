c32923d9132333559bf4c07ce96042a5d7ef92d9
RexshackGaming <rexshackuk@gmail.com>
2023-10-28 07:13:05 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -92,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `telegrams` (
 CREATE TABLE IF NOT EXISTS `player_horses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) NOT NULL,
-  `horseid` varchar(6) NOT NULL,
+  `horseid` varchar(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `horse` varchar(50) NOT NULL,
   `horsexp` int(11) DEFAULT 0,
