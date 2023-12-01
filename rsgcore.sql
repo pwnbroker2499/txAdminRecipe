7c21c5baea4dc398a752fe7f6820c0b45ba47500
RexshackGaming <rexshackuk@gmail.com>
2023-12-01 23:31:11 +0000

    remove ranch from build

--- rsgcore.sql
+++ rsgcore.sql
@@ -501,43 +501,6 @@ INSERT INTO `doors` (`doorid`, `doorstate`) VALUES
 ('1535511805', 1),
 ('2395304827', 1);
 
-CREATE TABLE IF NOT EXISTS `ranch_animals` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `ranchid` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
-    `animalid` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
-    `animals` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `ranch_stock` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jobaccess` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `item` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `stock` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `ranch_shop` (
-  `shopid` varchar(255) NOT NULL,
-  `jobaccess` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `displayname` varchar(255) NOT NULL,
-  `money` double(11,2) NOT NULL DEFAULT 0.00,
-  PRIMARY KEY (`shopid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO `ranch_shop` (`shopid`, `jobaccess`, `displayname`, `money`) VALUES
-('macfarranchshop', 'macfarranch', 'Macfarlan Ranch Shop', 0),
-('prongranchshop', 'prongranch', 'Pronghorn Ranch Shop', 0);
-
-CREATE TABLE IF NOT EXISTS `ranch_shop_stock` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `shopid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `items` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `stock` int(11) NOT NULL DEFAULT 0,
-  `price` double(11,2) NOT NULL DEFAULT 0.00,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 CREATE TABLE IF NOT EXISTS `hunting_wagons` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `citizenid` varchar(50) DEFAULT NULL,
