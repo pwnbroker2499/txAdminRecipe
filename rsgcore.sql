8f214c51993b0c41f9021d571c9c7c2729e7862e
RexshackGaming <rexshackuk@gmail.com>
2023-12-24 10:22:39 +0000

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -526,3 +526,16 @@ CREATE TABLE IF NOT EXISTS `hunting_inventory` (
     `plate` varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `treasure` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `looted` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `treasure` (`id`, `name`, `looted`) VALUES
+(1, 'treasure1', 0),
+(2, 'treasure2', 0),
+(3, 'treasure3', 0),
+(4, 'treasure4', 0);
