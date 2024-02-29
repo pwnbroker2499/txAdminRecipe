7a0774894729ed174a656cecd4276c592fe6fdab
pwnbroker2499 <dlambert2499@gmail.com>
2024-02-28 18:04:01 -0700

    typo

--- rsgcore.sql
+++ rsgcore.sql
@@ -42,14 +42,6 @@ CREATE TABLE IF NOT EXISTS `playerskins` (
   INDEX `citizenid` (`citizenid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `playeroutfit` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `citizenid` VARCHAR(50) NOT NULL COLLATE 'utf8mb3_general_ci',
-  `name` VARCHAR(255) NOT NULL COLLATE 'utf8mb3_general_ci',
-  `clothes` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS `stashitems` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `stash` varchar(255) NOT NULL DEFAULT '[]',
